3799996aeb82f73ef483eb07965468ce733011f5
Mateusz Sakowicz <mateusz19890121@gmail.com>
2020-02-26 16:39:01 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,34 +1,86 @@
-<?php
-//require_once "connect.php"
-//$connection = @new mysqli($host, $db_user, $db_password, $db_name);
-//if($connection->connect_erno!=0)
-//{
-//    echo "Error: ".$connection->connect_erno . "Opis" . $connection->connect_error;
-//}
-//else
-//{
+<?php 
 //    $name = $_POST['name'];
 //    $surname = $_POST['surname'];
 //    echo 'It works!';
  //   $connection->close();
 //}
-$host='localhost';
-$user='root';
-$pass='';
-$db='baza1';
-$con = mysqli_connect($host, $user, $pass, $db);
-if($con)
+
+
+// try
+// {
+//     $con = new PDO('mysql:host=localhost;dbname=formdata', 'root', '');
+//     try
+// catch (PDOException $e)
+// {
+//     print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
+//     die();
+// }
+// if($con)
+// {
+//     echo 'Successsss';
+// }
+
+
+try
 {
-    echo 'Successsss';
-}
+
 $name = $_POST['name'];
 $surname = $_POST['surname'];
-$sql = "insert into tabela (Name, Surname) VALUES ('$name', '$surname')";
-$query=mysqli_query($con,$sql);
-if($query)
-    echo 'data inserted';
-header("refresh:8; url=index.html");
+$email = $_POST['email'];
+$prefix = $_POST['prefix'];
+$tel = $_POST['telephone'];
+$telephone = $_POST['prefix'] . $_POST['telephone'];
+$birthDate = $_POST['birthDate'];
+$height = $_POST['height'];
+$weight = $_POST['weight'];
+$country = $_POST['country'];
+$gender = $_POST['gender'];
 
+    $pdo = new PDO('mysql:host=localhost;dbname=formdata', 'root', '');
+    $dodaj = "INSERT INTO data (Name, Surname, Email, Telephone, Date_of_Birth, Height, Weight, Gender, Country)
+    VALUES ('$name', '$surname', '$email', '$telephone', '$birthDate', '$height', '$weight', '$gender', '$country' )";
+    $stmt = $pdo->prepare($dodaj); 
+    $stmt->bindParam(":Name", $name, PDO::PARAM_STR);
+    $stmt->bindParam(":Surname", $surname, PDO::PARAM_STR);
+    $stmt->bindParam(":Email", $email, PDO::PARAM_STR);
+    $stmt->bindParam(":Telephone", $telephone, PDO::PARAM_INT);
+    $stmt->bindParam(":Date_of_birth", $birthDate, PDO::PARAM_INT);
+    $stmt->bindParam(":Height", $height, PDO::PARAM_INT);
+    $stmt->bindParam(":Weight", $weight, PDO::PARAM_INT);
+    $stmt->bindParam(":Country", $country, PDO::PARAM_STR);
+    $stmt->bindParam(":Gender", $gender, PDO::PARAM_STR);
+    $stmt->execute();
+    if ($stmt > 0) {
+        echo "Dodano: ".$dodaj." wierszy<br/> ";
+    }
+    else
+    {
+        echo "Blad podczas dodawania danych!<br/>";
+    }
+
+    $pdo = null;
+}
+catch (PDOException $e){
+    echo 'Blad polaczenia:' . $e->getMessage();
+}
+header("refresh:3; url=table.php");
+
+
+// $sql = "insert into data (Name, Surname, Email, Telephone, Date of Birth, Height, Weight, Gender, Country) 
+// VALUES ('$name', '$surname', '$email', "$prefix . ' ' . $telephone", '$birthDate', '$hieght', '$weight', '$male . $female', '$country' )";
+// $query=mysqli_query($con,$sql);
+// if($query)
+//     echo 'data inserted';
+// header("refresh:8; url=index.html");
+
+
+// $statement = $db->prepare("INSERT INTO tabela (imie, nazwisko) VALUES (:imie, :nazwisko)");
+// foreach($data as $row)
+// {
+//     $statement->bindValue(':imie', $row['imie'], PDO::PARAM_STR);
+//     $statement->bindValue(':nazwisko', $row['nazwisko'], PDO::PARAM_STR);
+//     $statement->execute();
+// }
 ?>
 
 
---
+++ table.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php   
+// echo phpinfo();
+// try
+// {
+   $dbh = new PDO("mysql:host=localhost;dbname=formdata", "root");
+// //    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+//    $sql ="select Name from data;";
+//    $statement = $pdo->prepare($sql);
+//    $statement->execute();
+
+//    var_dump($statement);
+
+$sth = $dbh->prepare("SELECT * FROM data");
+$sth->execute();
+
+/* Fetch all of the values of the first column */
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+echo "<table>";
+foreach($result as $key => $row)
+{
+    
+    echo "<tr>";
+    echo "<td>".$row['Name'] . ' ' . $row['Surname']. ' ' . $row['Email'] . ' ' . $row['Telephone'] . ' ' . $row['Date_of_Birth'] . ' ' . $row['Height'] . ' '. $row['Weight'] . ' ' . $row['Gender'] . ' '. $row['Country'] . ' ' ."</td>";
+    echo "<tr>";
+    
+
+}
+echo "</table>";
+   ​
+//    $result = $statement->fetchAll();
+
+// }
+// catch(PDOException $e)
+// {
+//    echo 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage();
+// }
+
+
+
+
+    // $servername = "localhost";
+    // $username = "";
+    // $password = "";
+    
+    // // Create connection
+    // $conn = mysqli_connect($servername, $username, $password);
+    // $pdo = new PDO('mysql:host=localhost;dbname=formdata', 'root', '');
+    // // Check connection
+    // if (!$pdo) {
+    //     die("Connection failed: " . mysqli_connect_error());
+    
+    // echo "Connected successfully";
+    // $query="SELECT * FROM message";
+    // mysql_select_db("formdat");
+    // $data=mysql_query($query);
+    // while($row=mysql_fetch_array($data)){
+    //     echo "<p>";
+    //     echo $row["id"];
+    //     echo "-"; // a separator
+    //     echo $row["name"];
+    //     echo "-"; // a separator
+    //     echo $row ["surname"];
+    //     echo "-"; // a separator
+    //     echo $row["email"];
+    //     echo "-"; // a separator
+    //     echo $row["telephone"];
+    //     echo "-"; // a separator
+    //     echo $row["date_of_birth"];
+    //     echo "-"; // a separator
+    //     echo $row["height"];
+    //     echo "-"; // a separator
+    //     echo $row["weight"];
+    //     echo "-"; // a separator
+    //     echo $row["gender"];
+    //     echo "-"; // a separator
+    //     echo $row["country"];
+    //     echo "</p>";
+    //     }
+
+    //     }else{
+    //     echo "<p>MySQL does not know that username and password </p>";
+    //     }
+
+//     echo "<tabel>
+//     <tr>
+//       <td>Name:</td>
+//       <td>".$_SESSION['name']."</td>
+//     </tr>
+//     <tr>
+//       <td>Surname:</td>
+//       <td></td>
+//     </tr>
+//     <tr>
+//       <td>Email</td>
+//       <td></td>
+//     </tr>
+//     <tr>
+//       <td>Telephone</td>
+//       <td></td>
+//     </tr>
+//     <tr>
+//       <td>Date of Birth</td>
+//       <td></td>
+//     </tr>
+//     <tr>
+//       <td>Height</td>
+//       <td></td>
+//     </tr>
+//     <tr>
+//       <td>Weight</td>
+//       <td></td>
+//     </tr>
+//     <tr>
+//       <td>Gender</td>
+//       <td></td>
+//     </tr>
+//     <tr>
+//       <td>Country</td>
+//       <td></td>
+//     </tr>
+//   </tabel>";
+?>
+
+
+</body>
+</html>
